3105d2a5bee5b24e7e5969dc90a84eca8ba6b57f
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-06-12 10:40:13 +0300

    made changes

--- trefle_api.js
+++ trefle_api.js
@@ -1,85 +1,122 @@
-const baseUrl = "https://trefle.io/api/v1/plants?token=IStUS451yET7aU6HoqFv5cLSGc_IEP64VIcL1Bssm6Q";
-const url = "https://corsproxy.io/?" + encodeURIComponent(baseUrl);
-// async function fetchPlants() {
-//     try {
-//         const response = await fetch(`${url}`);
-//         const plants = await response.json();
-//         displayPlants(plants.data.slice(0, 14)); 
-//         const searchBar = document.getElementById('searchBar');
-//         searchBar.addEventListener('input', () => filterPlants(plants.data));
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-
-async function fetchPlants() {
-    try {
-        const totalPages = 150;
-        const plantsData = [];
-        for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-            const response = await fetch(`${url}&page=${currentPage}`);
-            const pageData = await response.json();
-            plantsData.push(...pageData.data);
+const baseUrl =
+        "https://trefle.io/api/v1/plants?token=IStUS451yET7aU6HoqFv5cLSGc_IEP64VIcL1Bssm6Q";
+      const url = "https://corsproxy.io/?" + encodeURIComponent(baseUrl);
+      let currentPage = 1;
+      let currentSearchTerm = "";
+      let currentFilterValue = "";
+      async function fetchPlants(page = 1, searchTerm = "", filterValue = "") {
+        try {
+          const response = await fetch(
+            `${url}&page=${page}&q=${encodeURIComponent(searchTerm)}&filter[${filterValue}]=${encodeURIComponent(searchTerm)}`,
+          );
+          const plants = await response.json();
+          displayPlants(plants.data, searchTerm);
+          updatePagination(plants.links);
+          return plants;
+        } catch (error) {
+          console.log(error);
+          return null;
         }
-        displayPlants(plantsData.slice(0, 40000));
-        const searchBar = document.getElementById('searchBar');
-        searchBar.addEventListener('input', () => filterPlants(plantsData));
-    } catch (error) {
-        console.log(error);
-    }
-}
-function displayPlants(plants) {
-    const container = document.querySelector('#container');
-    container.innerHTML = ''; 
-    plants.forEach(plant => {
-        const plantCard = document.createElement('div');
-        plantCard.classList.add('plant-card');
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = plant.common_name;
-        plantCard.appendChild(nameElement);
-        const imgElement = document.createElement('img');
-        imgElement.src = plant.image_url;
-        imgElement.alt = plant.common_name;
-        plantCard.appendChild(imgElement);
-        // Add event listener for displaying details on click
-        plantCard.addEventListener('click', () => displayPlantDetails(plant));
-        container.appendChild(plantCard);
-    });
-}
-function filterPlants(plants) {
-    const searchTerm = document.getElementById('searchBar').value.toLowerCase();
-    const filteredPlants = plants.filter(plant =>
-        plant.common_name && plant.common_name.toLowerCase().includes(searchTerm)
-    );
-    displayPlants(filteredPlants.slice(0, 14));
-}
-function displayPlantDetails(plant) {
-    const modal = document.getElementById('modal');
-    const plantDetails = document.getElementById('plant-details');
-
-    plantDetails.innerHTML = `
-        <h2>${plant.common_name}</h2>
-        <img src="${plant.image_url}" alt="${plant.common_name}" style="width:100%;height:auto;border-radius:15px;">
-        <p><strong>Scientific Name:</strong> ${plant.scientific_name}</p>
-        <p><strong>Family:</strong> ${plant.family_common_name}</p>
-        <p><strong>Genus:</strong> ${plant.genus}</p>
-        <p><strong>Year:</strong> ${plant.year}</p>
-        <p><strong>Bibliography:</strong> ${plant.bibliography}</p>
-    `;
-  
-    modal.style.display = "block";
-    const closeButton = document.querySelector('.close-button');
-    closeButton.addEventListener('click', () => {
-        modal.style.display = "none";
-    });
-    window.addEventListener('click', (event) => {
-        if (event.target == modal) {
+      }
+      function displayPlants(plants, searchTerm = "") {
+        const container = document.querySelector("#container");
+        container.innerHTML = ""; // Clear previous results
+        if (
+          plants.length === 0 ||
+          (searchTerm &&
+            !plants.some((plant) =>
+              plant.common_name.toLowerCase().includes(searchTerm),
+            ))
+        ) {
+          const notFoundMessage = document.createElement("p");
+          notFoundMessage.textContent = "No plants found.";
+          container.appendChild(notFoundMessage);
+        } else {
+          plants.forEach((plant) => {
+            if (
+              !searchTerm ||
+              plant.common_name.toLowerCase().includes(searchTerm)
+            ) {
+              const plantCard = document.createElement("div");
+              plantCard.classList.add("plant-card");
+              const nameElement = document.createElement("h3");
+              nameElement.textContent = plant.common_name;
+              plantCard.appendChild(nameElement);
+              const imgElement = document.createElement("img");
+              imgElement.src = plant.image_url;
+              imgElement.alt = plant.common_name;
+              plantCard.appendChild(imgElement);
+              plantCard.addEventListener("click", () =>
+                displayPlantDetails(plant),
+              );
+              container.appendChild(plantCard);
+            }
+          });
+        }
+      }
+      function updatePagination(links) {
+        const prevPageButton = document.getElementById("prevPage");
+        const nextPageButton = document.getElementById("nextPage");
+        if (links.prev) {
+          prevPageButton.disabled = false;
+        } else {
+          prevPageButton.disabled = true;
+        }
+        if (links.next) {
+          nextPageButton.disabled = false;
+        } else {
+          nextPageButton.disabled = true;
+        }
+      }
+      function displayPlantDetails(plant) {
+        const modal = document.getElementById("modal");
+        const plantDetails = document.getElementById("plant-details");
+        plantDetails.innerHTML = `
+                <h2>${plant.common_name}</h2>
+                <img src="${plant.image_url}" alt="${plant.common_name}" style="width:100%;height:auto;border-radius:15px;">
+                <p><strong>Scientific Name:</strong> ${plant.scientific_name}</p>
+                <p><strong>Family:</strong> ${plant.family_common_name}</p>
+                <p><strong>Genus:</strong> ${plant.genus}</p>
+                <p><strong>Year:</strong> ${plant.year}</p>
+                <p><strong>Bibliography:</strong> ${plant.bibliography}</p>
+            `;
+        modal.style.display = "block";
+        const closeButton = document.querySelector(".close-button");
+        closeButton.addEventListener("click", () => {
+          modal.style.display = "none";
+        });
+        window.addEventListener("click", (event) => {
+          if (event.target == modal) {
             modal.style.display = "none";
-        }
-    });
-}
-fetchPlants();
+          }
+        });
+      }
+      function searchPlants() {
+        const searchTerm = document
+          .getElementById("searchBar")
+          .value.toLowerCase();
+        const filterSelect = document.getElementById("filterSelect");
+        const filterValue = filterSelect.value;
+        currentSearchTerm = searchTerm;
+        currentFilterValue = filterValue;
+        currentPage = 1;
+        fetchPlants(currentPage, searchTerm, filterValue);
+      }
+      const searchBar = document.getElementById("searchBar");
+      searchBar.addEventListener("input", searchPlants);
+      const filterSelect = document.getElementById("filterSelect");
+      filterSelect.addEventListener("change", searchPlants);
+      const prevPageButton = document.getElementById("prevPage");
+      prevPageButton.addEventListener("click", () => {
+        currentPage--;
+        fetchPlants(currentPage, currentSearchTerm, currentFilterValue);
+      });
+      const nextPageButton = document.getElementById("nextPage");
+      nextPageButton.addEventListener("click", () => {
+        currentPage++;
+        fetchPlants(currentPage, currentSearchTerm, currentFilterValue);
+      });
+      fetchPlants(currentPage);
 
 
 
